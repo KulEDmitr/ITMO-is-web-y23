6b9179f237c111884157b7e446cc6e3399754401
Ekaterina <242371@niuitmo.ru>
2022-03-24 01:40:13 +0300

    handlebars with layout added

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,22 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.setViewEngine('hbs');
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
+  app.set('view options', { layout: 'layouts/layout' });
+
+  hbs.registerPartials(join(__dirname, '..', 'views/partials'));
+  hbs.registerPartials(join(__dirname, '..', 'views/pages'));
 
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 3000;
   await app.listen(port);
 }
+
 bootstrap();
