9929464532231dfd9961df31752a172b53be5c29
Ekaterina <242371@niuitmo.ru>
2022-03-24 01:41:31 +0300

    little fixes for static

--- public/scripts/form.js
+++ public/scripts/form.js
@@ -1,93 +1,97 @@
-const addTaskBtn = document.getElementById("add");
-const taskInput = document.getElementById("task__text");
-const formResult= document.querySelector(".form__result");
+const addTaskBtn = document.getElementById('add');
+const taskInput = document.getElementById('task__text');
+const formResult = document.querySelector('.form__result');
 const templateItem = document.getElementById('template__todo__item');
 
 let tasks;
 let todoItemElements = [];
 
 function Task(description) {
-    this.description = description;
-    this.completed = false;
+  this.description = description;
+  this.completed = false;
 }
 
-localStorage.tasks ?  tasks = JSON.parse(localStorage.getItem('tasks')) : tasks = [];
+localStorage.tasks
+  ? (tasks = JSON.parse(localStorage.getItem('tasks')))
+  : (tasks = []);
 
 const createTemplate = (task, index) => {
-    let template = templateItem.content.cloneNode(true);
+  let template = templateItem.content.cloneNode(true);
 
-    template.getElementById('task__text').innerText = task.description;
-    
-    let checkbox__done = template.getElementById('checkbox__done');
-    checkbox__done.onclick = () => {
-        completeTask(index);
-    }
-    template.getElementById('button__delete').onclick = () => {
-        deleteTask(index);
-    }
+  template.getElementById('task__text').innerText = task.description;
 
-    if (task.completed) {
-        checkbox__done.checked = true;
-        template.getElementById('todo__item').classList.add('checked');
-    }
+  let checkbox__done = template.getElementById('checkbox__done');
+  checkbox__done.onclick = () => {
+    completeTask(index);
+  };
+  template.getElementById('button__delete').onclick = () => {
+    deleteTask(index);
+  };
 
-    return template;
-}
+  if (task.completed) {
+    checkbox__done.checked = true;
+    template.getElementById('todo__item').classList.add('checked');
+  }
+
+  return template;
+};
 
 const filterTasks = () => {
-    const activeTasks = tasks.length && tasks.filter(item => item.completed == false);
-    const completedTasks = tasks.length && tasks.filter(item => item.completed == true);
-    tasks = [...activeTasks , ...completedTasks];
-}
+  const activeTasks =
+    tasks.length && tasks.filter((item) => item.completed === false);
+  const completedTasks =
+    tasks.length && tasks.filter((item) => item.completed === true);
+  tasks = [...activeTasks, ...completedTasks];
+};
 
 const fillHtmlList = () => {
-    formResult.innerHTML = "";
-    if(tasks.length > 0 ) {
-        filterTasks();
-        tasks.forEach((item, index) => {
-            formResult.appendChild(createTemplate(item, index));
-        });
-        todoItemElements = document.querySelectorAll('.todo__item')
-    }
-}
+  formResult.innerHTML = '';
+  if (tasks.length > 0) {
+    filterTasks();
+    tasks.forEach((item, index) => {
+      formResult.appendChild(createTemplate(item, index));
+    });
+    todoItemElements = document.querySelectorAll('.todo__item');
+  }
+};
 
 fillHtmlList();
 
 const updateLocal = () => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-}
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
 
 const updateData = () => {
-    updateLocal();
-    fillHtmlList();
-}
+  updateLocal();
+  fillHtmlList();
+};
 
 addTaskBtn.addEventListener('click', () => {
-    let text = taskInput.value
-    if (text.length == 0) {
-        taskInput.classList.add('wrong')
-    } else {
-        taskInput.classList.remove('wrong')
-        tasks.push(new Task(text));
-        updateData();
-        taskInput.value ="";
-    }
-})
-
-const completeTask = index => {
-    tasks[index].completed = !tasks[index].completed;
-    if (tasks[index].completed) {
-        todoItemElements[index].classList.add('checked');
-    } else {
-        todoItemElements[index].classList.remove('checked');
-    }
+  let text = taskInput.value;
+  if (text.length === 0) {
+    taskInput.classList.add('wrong');
+  } else {
+    taskInput.classList.remove('wrong');
+    tasks.push(new Task(text));
     updateData();
-}
+    taskInput.value = '';
+  }
+});
 
-const deleteTask = index => {
-    todoItemElements[index].classList.add('delition');
-    setTimeout(() => {
-        tasks.splice(index, 1);
-        updateData();
-    }, 500)
-}
+const completeTask = (index) => {
+  tasks[index].completed = !tasks[index].completed;
+  if (tasks[index].completed) {
+    todoItemElements[index].classList.add('checked');
+  } else {
+    todoItemElements[index].classList.remove('checked');
+  }
+  updateData();
+};
+
+const deleteTask = (index) => {
+  todoItemElements[index].classList.add('delition');
+  setTimeout(() => {
+    tasks.splice(index, 1);
+    updateData();
+  }, 500);
+};
