5ff5e6cabcd05610e7e98e2e74445d1335b7c784
Ekaterina <242371@niuitmo.ru>
2022-03-24 01:42:22 +0300

    controllers  for rendering added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,56 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Render } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render("pages/index1")
+  root() {
+    return {};
+  }
+
+  @Get("different")
+  @Render("pages/different")
+  different() {
+    return {};
+  }
+
+  @Get("form")
+  @Render("pages/form")
+  form() {
+    return {
+      add_styles : '<link rel="stylesheet" href ="css/form.css">'
+    };
+  }
+
+  @Get("gallery")
+  @Render("pages/gallery")
+  gallery() {
+    return {
+      add_styles: '<!-- swiper -->' +
+        '<link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>' +
+        '<link rel="stylesheet" href ="/css/gallery.css">'
+    };
+  }
+
+  @Get("loaded_pictures")
+  @Render("pages/loaded_pictures")
+  loaded_pictures() {
+    return {
+      add_styles: '<link rel="stylesheet" href ="/css/loaded.css">'
+    };
+  }
+
+  @Get("operation")
+  @Render("pages/operation")
+  operation() {
+    return {};
+  }
+
+  @Get("physics")
+  @Render("pages/physics")
+  physics() {
+    return {};
   }
 }
