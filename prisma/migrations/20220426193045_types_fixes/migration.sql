536e079179b797859dce976581a05af94fcd85bd
Ekaterina <242371@niuitmo.ru>
2022-04-27 12:44:24 +0300

    prisma schema fixed

--- prisma/migrations/20220426193045_types_fixes/migration.sql
+++ prisma/migrations/20220426193045_types_fixes/migration.sql
@@ -3,12 +3,7 @@
 
   - The primary key for the `CategoryOnPictures` table will be changed. If it partially fails, the table could be left without primary key constraint.
   - The primary key for the `CategoryOnPosts` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to alter the column `title` on the `Picture` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
-  - You are about to alter the column `name` on the `PictureCategory` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
-  - You are about to alter the column `title` on the `Post` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
-  - You are about to alter the column `name` on the `PostCategory` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
   - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to alter the column `name` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
   - A unique constraint covering the columns `[login]` on the table `User` will be added. If there are existing duplicate values, this will fail.
   - Made the column `picId` on table `CategoryOnPictures` required. This step will fail if there are existing NULL values in that column.
   - Made the column `catId` on table `CategoryOnPictures` required. This step will fail if there are existing NULL values in that column.
@@ -16,6 +11,7 @@
   - Changed the type of `authorId` on the `Post` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
   - Added the required column `login` to the `User` table without a default value. This is not possible if the table is not empty.
   - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Changed the type of `id` on the `User` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
 
 */
 -- DropForeignKey
@@ -35,30 +31,20 @@ ALTER TABLE "CategoryOnPosts" DROP CONSTRAINT "CategoryOnPosts_pkey",
 ADD CONSTRAINT "CategoryOnPosts_pkey" PRIMARY KEY ("postId", "catId");
 
 -- AlterTable
-ALTER TABLE "Picture" ALTER COLUMN "title" SET DATA TYPE VARCHAR(255),
-DROP COLUMN "ownerId",
+ALTER TABLE "Picture" DROP COLUMN "ownerId",
 ADD COLUMN     "ownerId" UUID NOT NULL;
 
 -- AlterTable
-ALTER TABLE "PictureCategory" ALTER COLUMN "name" SET DATA TYPE VARCHAR(255);
-
--- AlterTable
-ALTER TABLE "Post" ALTER COLUMN "title" SET DATA TYPE VARCHAR(255),
-DROP COLUMN "authorId",
+ALTER TABLE "Post" DROP COLUMN "authorId",
 ADD COLUMN     "authorId" UUID NOT NULL;
 
 -- AlterTable
-ALTER TABLE "PostCategory" ALTER COLUMN "name" SET DATA TYPE VARCHAR(255);
-
--- AlterTable
 ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
-ADD COLUMN     "login" VARCHAR(255) NOT NULL,
-ADD COLUMN     "password" VARCHAR(255) NOT NULL,
-ALTER COLUMN "id" DROP DEFAULT,
-ALTER COLUMN "id" SET DATA TYPE TEXT,
-ALTER COLUMN "name" SET DATA TYPE VARCHAR(255),
+ADD COLUMN     "login" TEXT NOT NULL,
+ADD COLUMN     "password" TEXT NOT NULL,
+DROP COLUMN "id",
+ADD COLUMN     "id" UUID NOT NULL,
 ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
-DROP SEQUENCE "User_id_seq";
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_login_key" ON "User"("login");
