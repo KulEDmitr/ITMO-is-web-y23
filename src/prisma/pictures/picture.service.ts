5a80ce7c155bfdf0c3f282d1839ef663c5bcde79
Ekaterina <242371@niuitmo.ru>
2022-05-07 19:27:26 +0300

    picture exception filter added

--- src/prisma/pictures/picture.service.ts
+++ src/prisma/pictures/picture.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
-import { Picture, Prisma } from '@prisma/client';
+import { Picture, Prisma, User } from '@prisma/client';
 import { CreatePictureDto } from './models/create-picture.dto';
 import { UpdatePictureDto } from './models/update-picture.dto';
+import { NotFoundException } from '../../exceptions/not-found.exception';
 
 @Injectable()
 export class PictureService {
@@ -25,6 +26,11 @@ export class PictureService {
   }
 
   async createPicture(data: CreatePictureDto): Promise<Picture | null> {
+    const exist_cat = await this.checkCategories(data.categories);
+    if (data.ownerId != undefined) {
+      await this.checkUser({ id: data.ownerId });
+    }
+
     return this.prisma.picture.create({
       data: {
         title: data.title,
@@ -34,7 +40,7 @@ export class PictureService {
           connect: { id: data.ownerId },
         },
         categories: {
-          create: data.categories?.map((cat) => ({
+          create: exist_cat?.map((cat) => ({
             category: {
               connect: { id: cat },
             },
@@ -55,6 +61,9 @@ export class PictureService {
     where: Prisma.PictureWhereUniqueInput,
     data: UpdatePictureDto,
   ): Promise<Picture | null> {
+    const exist_cat = await this.checkCategories(data.categories);
+    await this.checkPicture(where);
+
     return this.prisma.picture.update({
       where,
       data: {
@@ -62,7 +71,7 @@ export class PictureService {
         image: data.image,
         description: data.description,
         categories: {
-          create: data.categories?.map((cat) => ({
+          create: exist_cat?.map((cat) => ({
             category: {
               connect: { id: cat },
             },
@@ -82,5 +91,38 @@ export class PictureService {
     return this.prisma.picture.delete({
       where,
     });
+  }
+
+  private async checkCategories(
+    categories?: number[],
+  ): Promise<number[] | null> {
+    if (categories == undefined) {
+      return categories;
+    }
+    const exist_cats = categories?.filter(
+      (cat) =>
+        this.prisma.pictureCategory.findUnique({ where: { id: cat } }) != null,
+    );
+    return exist_cats.length == 0 ? undefined : exist_cats;
+  }
+
+  private async checkUser(where: Prisma.UserWhereUniqueInput) {
+    const user: User | null = await this.prisma.user.findUnique({
+      where,
+    });
+    if (user == null) {
+      throw new NotFoundException(
+        'User with data: "' + where + '" does not exist',
+      );
+    }
+  }
+
+  private async checkPicture(where: Prisma.PictureWhereUniqueInput) {
+    const pic: Picture | null = await this.prisma.picture.findUnique({
+      where,
+    });
+    if (pic == null) {
+      throw new NotFoundException('Picture with given data does not exist');
+    }
   }
 }
